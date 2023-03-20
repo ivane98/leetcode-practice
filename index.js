cafa38b02b7f3a3306b06cad7ebd2dc590611091
ivane98 <i.vardoshvili22@gmail.com>
2023-03-20 11:24:13 +0400

    20

--- index.js
+++ index.js
@@ -30,3 +30,52 @@
 
 //   return root;
 // };
+
+//118. Pascal's Triangle
+
+// var generate = function (numRows) {
+//   if (numRows < 1) return [];
+//   if (numRows === 1) return [[1]];
+
+//   const triangle = [[1]];
+
+//   for (let i = 1; i < numRows; i++) {
+//     let prevRow = triangle[i - 1];
+//     let currRow = [];
+
+//     currRow.push(1);
+
+//     for (let j = 1; j < prevRow.length; j++) {
+//       currRow[j] = prevRow[j] + prevRow[j - 1];
+//     }
+
+//     currRow.push(1);
+
+//     triangle.push(currRow);
+//   }
+//   return triangle;
+// };
+
+// 121. Best Time to Buy and Sell Stock
+
+// var maxProfit = function (prices) {
+//   let sellIdx = prices.length - 1;
+//   let profit = 0;
+
+//   for (let buyIdx = prices.length - 1; buyIdx >= 0; buyIdx--) {
+//     let sellVal = prices[sellIdx];
+//     let buyVal = prices[buyIdx];
+
+//     if (buyVal - sellVal >= 0) {
+//       sellIdx = buyIdx;
+//     } else {
+//       let price = sellVal - buyVal;
+
+//       profit = Math.max(profit, price);
+//     }
+//   }
+
+//   return profit;
+// };
+
+// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
