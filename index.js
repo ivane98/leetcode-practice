d72872548caed0cbd71b4bf7d79f491cadfd419f
ivane98 <i.vardoshvili22@gmail.com>
2023-04-11 11:18:49 +0400

    m

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//49. Group Anagrams
 
+var groupAnagrams = function(strs) {
+    const sortedStrs = strs.map(word => word.split('').sort().join(''))
+   const hash = {}
+
+   for(let i = 0; i < strs.length; i++){
+     if(!hash[sortedStrs[i]]){
+       hash[sortedStrs[i]] = [strs[i]]
+     }
+     else {
+       hash[sortedStrs[i]].push(strs[i])
+     }
+   }
+
+   return Object.values(hash)
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
